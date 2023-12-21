bd39f99445653c758f1012659fc5675409b752b9
Luck0332 <152171778+Luck0332@users.noreply.github.com>
2023-12-21 17:36:58 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,8 @@
 <html>
     <body>
         <h1>this is PHP <?php echo 'Hello Word!' ;?> </h1>
+        <?php $var_a = "SE CAME" ?>
+        <?php echo $var_b = 'Hello $var_a' ?>
+        <?php echo $var_c = "Hello $var_a" ?>
     </body>
 </html>
