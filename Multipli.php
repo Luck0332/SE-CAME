a657f718a5c575be734bd052e9bc8d2ef33bd4ff
Luck0332 <152171778+Luck0332@users.noreply.github.com>
2023-12-23 23:30:20 +0700

    update_3

--- Multipli.php
+++ Multipli.php
@@ -8,38 +8,40 @@
     <div class="main">
         
         <div class="main_two">
-            <?php
-            $my_val = isset($_POST['my_val']) ? $_POST['my_val'] : ''; // แก้ไขตรวจสอบค่าจากฟอร์ม
-            ?>
-            <h1>ตารางสูตรคูณ </h1>
-            <br>
-            <form method="post">
-                <label for="my_val">โปรดเลือกแม่ : </label>
-                <select name="my_val" id="my_val">
-                    <?php
-                    // เพิ่มตัวเลือกแม่
-                    for ($i = 1; $i <= 1000; $i++) {
-                        echo "<option value=\"$i\">$i</option>";
-                    }
-                    ?>
-                </select>
-                <button type="submit"> ตกลง</button>
-            </form>
-            <br>
-            <div class="table_scroll">
-                <table class="table_1">
-                    <tbody>
-                        <?php for  ($j = 1; $j <= 24; $j++) { ?>
-                            <tr>
-                                <td><?php echo $my_val ?></td>
-                                <td> x </td>
-                                <td><?php echo $j ?></td>
-                                <td> = </td>
-                                <td><?php echo $my_val * $j ?></td>
-                            </tr>
-                        <?php } ?>
-                    </tbody>
-                </table>
+            <div>
+                <?php
+                $my_val = isset($_POST['my_val']) ? $_POST['my_val'] : ''; // แก้ไขตรวจสอบค่าจากฟอร์ม
+                ?>
+                <h1>ตารางสูตรคูณ </h1>
+                <br>
+                <form method="post">
+                    <label for="my_val">โปรดเลือกแม่ : </label>
+                    <select name="my_val" id="my_val">
+                        <?php
+                        // เพิ่มตัวเลือกแม่
+                        for ($i = 1; $i <= 1000; $i++) {
+                            echo "<option value=\"$i\">$i</option>";
+                        }
+                        ?>
+                    </select>
+                    <button type="submit"> ตกลง</button>
+                </form>
+                <br>
+                <div class="table_scroll">
+                    <table class="table_1">
+                        <tbody>
+                            <?php for  ($j = 1; $j <= 24; $j++) { ?>
+                                <tr>
+                                    <td><?php echo $my_val ?></td>
+                                    <td> x </td>
+                                    <td><?php echo $j ?></td>
+                                    <td> = </td>
+                                    <td><?php echo $my_val * $j ?></td>
+                                </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
 
@@ -77,8 +79,8 @@
             }
 
             .main_two{
-                width: 100%;
-                display: grid;
+                width: 120%;
+                display: flex;
                 justify-content: center;
                 align-items: center;
             }
